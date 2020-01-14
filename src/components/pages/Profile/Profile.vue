139b4c09cb297a77750ba3c0edc524fb5b5c6400
Vinicius Cainelli <vicainelli+github@gmail.com>
2020-01-14 01:07:59 -0300

    Update Profile page
    Update UI and add more language keys

--- src/components/pages/Profile/Profile.vue
+++ src/components/pages/Profile/Profile.vue
@@ -2,6 +2,7 @@
   <base-template :page-title="$t('profile')">
     <div v-if="didProfileLoad">
       <section-card>
+        <!-- <h2 class="uppercase tracking-wide text-sm text-gray-800">Personal Info</h2> -->
         <div class="flex items-center">
           <div class="mr-3">
             <img
@@ -17,20 +18,35 @@
           </div>
         </div>
 
-        <div class="my-6">gender: {{ getProfile.gender }}</div>
+        <div class="my-6">
+          <span class="uppercase text-xs tracking-wide text-gray-800">{{ $t('gender') }}</span> <br />
+          <span>{{ getProfile.gender }}</span>
+        </div>
 
-        <div class="my-6">emailAddress: {{ getProfile.emailAddress }}</div>
+        <div class="my-6">
+          <span class="uppercase text-xs tracking-wide text-gray-800">{{ $t('emailAddress') }}</span> <br />
+          <span>{{ getProfile.emailAddress }}</span>
+        </div>
 
-        <div class="my-6">address: {{ getProfile.address }}</div>
+        <div class="my-6">
+          <span class="uppercase text-xs tracking-wide text-gray-800">{{ $t('address') }}</span> <br />
+          <span>{{ getProfile.address }}</span>
+        </div>
 
-        <div class="my-6">birthday: {{ getProfile.birthday }}</div>
-      </section-card>
-      <section-card>
-        <h2 class="uppercase tracking-wide text-sm text-gray-800">App Settings</h2>
+        <div class="my-6">
+          <span class="uppercase text-xs tracking-wide text-gray-800">{{ $t('birthday') }}</span> <br />
+          <span><format-date-time :data="{ date: getProfile.birthday, format: 'MMM DD, YYYY' }"/></span>
+        </div>
 
-        <div class="my-6">language: {{ getProfile.language }}</div>
+        <div class="my-6">
+          <span class="uppercase text-xs tracking-wide text-gray-800">{{ $t('language') }}</span> <br />
+          <span>{{ getProfile.language }}</span>
+        </div>
 
-        <div class="my-6">currency: {{ getProfile.currency }}</div>
+        <div class="my-6">
+          <span class="uppercase text-xs tracking-wide text-gray-800">{{ $t('currency') }}</span> <br />
+          <span>{{ getProfile.currency }}</span>
+        </div>
       </section-card>
     </div>
     <div v-else><pulse-loader /></div>
@@ -38,6 +54,7 @@
 </template>
 
 <script>
+import FormatDateTime from '@/components/atoms/FormatDateTime'
 import SectionCard from '@/components/atoms/SectionCard'
 import PulseLoader from '@/components/atoms/PulseLoader'
 import BaseTemplate from '@/components/templates/BaseTemplate'
@@ -46,6 +63,7 @@ export default {
   name: 'page-profile',
   components: {
     'section-card': SectionCard,
+    'format-date-time': FormatDateTime,
     'base-template': BaseTemplate,
     'pulse-loader': PulseLoader
   },
