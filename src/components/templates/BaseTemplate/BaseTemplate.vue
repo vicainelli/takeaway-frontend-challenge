f26760ccf24f8e112e15d0b386cc96860c55804e
Vinicius Cainelli <vicainelli+github@gmail.com>
2020-01-14 01:09:58 -0300

    Adding BackButton to template

--- src/components/templates/BaseTemplate/BaseTemplate.vue
+++ src/components/templates/BaseTemplate/BaseTemplate.vue
@@ -7,8 +7,13 @@
     </div>
     <!-- Main content -->
     <div class="lg:flex-1">
-      <div class="px-4 py-6 mb-6 border-b bg-white ">
-        <h1 class="text-center lg:text-left uppercase tracking-wide">{{ pageTitle }}</h1>
+      <div class="px-4 py-6 mb-6 border-b bg-white relative ">
+        <div v-if="backButton" class="absolute w-3 lg:hidden">
+          <back-button class="text-gray-600" :path-to="backButton.pathTo"></back-button>
+        </div>
+        <div class="w-full">
+          <h1 class="text-center lg:text-left uppercase tracking-wide">{{ pageTitle }}</h1>
+        </div>
       </div>
       <div class="px-4 pb-12">
         <slot />
@@ -19,16 +24,22 @@
 </template>
 
 <script>
+import BackButton from '@/components/atoms/BackButton'
 import NavigationBar from '@/components/organisms/NavigationBar'
 export default {
   name: 'base-template',
   components: {
-    'navigation-bar': NavigationBar
+    'navigation-bar': NavigationBar,
+    'back-button': BackButton
   },
   props: {
     pageTitle: {
       type: String,
       default: ''
+    },
+    backButton: {
+      type: Object,
+      default: null
     }
   }
 }
