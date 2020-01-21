e1d7bac1be2f3b126048904c835e13d2979d37ff
Vinicius Cainelli <vicainelli+github@gmail.com>
2020-01-21 18:45:39 -0300

    Split files

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,7 @@
-import Home from '@/components/pages/Home'
-import Orders from '@/components/pages/Orders'
-import Profile from '@/components/pages/Profile'
-import OrderShow from '@/components/pages/OrderShow'
+const Home = () => import(/* webpackChunkName: "home" */ '@/components/pages/Home');
+const Orders = () => import(/* webpackChunkName: "orders" */ '@/components/pages/Orders');
+const Profile = () => import(/* webpackChunkName: "profile" */ '@/components/pages/Profile');
+const OrderShow = () => import(/* webpackChunkName: "order_show" */ '@/components/pages/OrderShow');
 
 const routes = [
   {
